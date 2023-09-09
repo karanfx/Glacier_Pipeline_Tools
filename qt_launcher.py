026cf396824dcbeb77afdf621ec813a447475709
karanfx <karan.fxartist@gmail.com>
2023-09-09 05:58:43 +0530

    setting up houdini.env variable

--- qt_launcher.py
+++ qt_launcher.py
@@ -61,6 +61,8 @@ class qt_launcher(ui.main_ui_6.Ui_MainWindow,QtWidgets.QMainWindow):
         self.seq_cB.currentTextChanged.connect(self.populate_shot)
         self.shot_cB.currentTextChanged.connect(self.set_dir)
 
+        #Launch apps
+        self.launch_button.clicked.connect(self.setup_env)
         self.launch_button.clicked.connect(self.opentool)
         self.manual_toolButton.clicked.connect(self.manual_dir)
 
@@ -230,6 +232,65 @@ class qt_launcher(ui.main_ui_6.Ui_MainWindow,QtWidgets.QMainWindow):
     def toolssetup(self):
         self.tools_cB.addItems(tooldata)
         
+    #Collect Selected Shot Data
+    def setup_env(self):
+        env_file = "C:/Users/PERMAN/Documents/houdini18.5/houdini.env"
+        search_text = "# Glacier Variables"
+        num_var = 6
+        
+        #Cleanup the file
+       # Read the file content into a list of lines
+        with open(env_file, 'r') as file:
+            lines = file.readlines()
+
+        # Create an empty list to store the modified content
+        modified_lines = []
+
+        i = 0
+        while i < len(lines):
+            line = lines[i]
+
+            if search_text in line:
+                # Found the search text, add it to the modified content
+                modified_lines.append(line)
+
+                # Skip the next lines_to_delete lines
+                for _ in range(num_var):
+                    i += 1
+            else:
+                modified_lines.append(line)
+
+            i += 1
+
+        # Write the modified content back to the file
+        with open(env_file, 'w') as file:
+            file.writelines(modified_lines)
+
+        print(f"Deleted {num_var} lines after each occurrence of '{search_text}'.")
+
+
+
+        #Setup Variables
+        toolname = self.tools_cB.currentText()
+        proj = self.project_cB.currentText()
+        seq = self.seq_cB.currentText()
+        shot = self.shot_cB.currentText()
+        shot_dir = self.manual_path_Ldit.text()
+        shot_dir = shot_dir.replace("\\","/")
+
+        job = os.path.join(shot_dir,toolname)
+
+        #Set env variables
+
+        variables = {"SHOW" : proj, "SEQ": seq, "SHOT":shot,"SHOT_DIR":shot_dir,"JOB":job}
+        with open(env_file,"a") as env:
+            env.write("# Glacier Variables\n")
+            for var,data in variables.items():
+                env.write(var +"="+ "\"" + data + "/" + "\"" + "\n")
+
+
+
+
     #open DCCs
     def opentool(self):
         toolname = self.tools_cB.currentText()
