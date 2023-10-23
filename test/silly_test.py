a64bad894b5d1e02dcc59f03c8eed456eebbade5
karanfx <karan.fxartist@gmail.com>
2023-10-23 06:29:25 +0530

    Maya Asset Importer in progress

--- test/silly_test.py
+++ test/silly_test.py
@@ -7,22 +7,7 @@
 vers = "v002"
 # n_vers = vers + 1
 
-import os
-import re
-versions = os.listdir("D:/test_studio/Show01/Seq_AB/Shot_AB001/libs/fx")
 
-numbers = [int(re.search(r'\d+', name).group(0)) for name in versions if re.search(r'\d+', name)]
-
-l = ["v001","v004","v005","v006"]
-max_v = str(max(l))
-max_v = max_v.strip('v')
-print(int(max_v)+1)
-
-max_v = str(max(l))
-max_v = int(max_v.strip('v')) +1
-print(int(max_v)+1)
-vers_up = f"v{max_v:03d}"
-print(vers_up)
-# print(max(versions))
-# print(versions[-1])
-# print(numbers)
+v = "\"safdfs\""
+print(v)
+print(v.strip("\""))
--- tools/maya/asset_importer.py
+++ tools/maya/asset_importer.py
@@ -1,9 +1,5 @@
 #Setup Env Variables 
 #Setup Imports using env variables
-
-
-
-
 from maya import cmds
 from maya import mel
 from maya import OpenMayaUI as omui 
@@ -21,7 +17,14 @@ mayaMainWindow = wrapInstance(long(mayaMainWindowPtr), QWidget)
 
 uiFile = "E:/Work/python_dev/QT_project_launcher/qt_ui_files/hou_tools_ui/scene_builder.ui"
 
-layout = "E:/Work/houdinifx/pipe_test/Show01/Seq_AB/Shot_AB001/layout/"
+# layout = "D:/test_studio/Show01/Seq_AB/Shot_AB001/libs/layout/"
+
+# get layout Dirs
+shot_dir = os.environ.get('SHOT_DIR')
+shot_dir = shot_dir.strip('\"')
+layout = os.path.join(shot_dir,'libs','layout')
+anim = os.path.join(shot_dir,'libs/anim')
+print(layout)
 
 
 class BuildScene(QWidget):    
@@ -43,11 +46,6 @@ class BuildScene(QWidget):
         self.ui.Build_PB.clicked.connect(self.build)
 
     def populate_assets(self):
-        #get layout Dirs
-        #shot_dir = os.environ.get('SHOT_DIR')
-        #layout = os.path.join(shot_dir,'libs/layout')
-        #anim = os.path.join(shot_dir,'libs/anim')
-        # print(layout)
 
         for sub in os.listdir(layout):
             # dirs = os.path.join(layout,sub)
@@ -59,52 +57,62 @@ class BuildScene(QWidget):
         self.ui.assets_LW.setDragDropMode(self.ui.assets_LW.InternalMove)
         self.ui.assets_LW.selectAll()
 
-    def get_layout_dirs(self):
-        shot_dir = os.environ.get('SHOT_DIR')
-        layout = os.path.join(shot_dir,'libs','layout')
-        anim = os.path.join(shot_dir,'libs','anim')
-        print(layout)
-
 
     def build(self):
-        # print("build")
-        #get layout Dirs
-        shot_dir = os.environ.get('SHOT_DIR')
-        # print(shot_dir)
-        layout = os.path.join(shot_dir,'libs','layout')
-        anim = os.path.join(shot_dir,'libs','anim')
-        # print(layout)
-
+        
         checked = []
         for index in range(self.ui.assets_LW.count()):
             item = self.ui.assets_LW.item(index)
             if item.checkState() == 2:
                 checked.append(item.text())
 
-        # print(checked)
-
         for sub in os.listdir(layout):
             for item in checked:
                 if item == sub:
-                    print('item',sub)
                     dirs = os.path.join(layout,sub)
-                    print('dirs',dirs)
-                    # for asset in os.listdir(dirs):
-                    #    asset_dirs = os.path.join(dirs,asset)
                     asset_dirs = dirs
                     ext = os.path.splitext(asset_dirs)[1]
                     name = os.path.basename(asset_dirs).replace(ext,"")
-                    print('name',name)
-                    print('asset_dir',asset_dirs)
 
-                    #call imports
-                    cmds.file(asset_dirs, i=True, type="FBX", ra=True, mergeNamespacesOnClash=False, namespace="FBXImport", options="v=0;", pr=True)
+                    #Call imports
+                    # Import the ABC file
+                    if ext == '.abc':
+                        cmds.AbcImport(asset_dirs, mode='import')
 
+                    # Import the OBJ file
+                    if ext == '.obj':
+                        cmds.file(asset_dirs, i=True, type="OBJ", ra=True, mergeNamespacesOnClash=False, namespace="OBJImport", options="mo=1;")
+
+                    # Import the FBX file
+                    if ext == '.fbx':
+                        cmds.file(asset_dirs, i=True, type="FBX", ra=True, mergeNamespacesOnClash=False, namespace="FBXImport", options="v=0;", pr=True)
+
+                    # Import the USD stage into Maya
+                    if ext == '.usd':
+                        from pxr import Usd, UsdGeom
+
+                        # Initialize the USD stage
+                        stage = Usd.Stage.Open(asset_dirs)
+                        namespace = "USDImport"
+                        cmds.usdImport(file=asset_dirs, shadingMode="none", primPath="/", mergeTransformAndShape=False, applyLayerOffset=False, name=namespace, asReference=False)
+
+                    # print(asset_dirs)
 
         #Setup Frame Range 
-        
+        start_frame = os.environ.get('G_START')
+        start_frame = int(start_frame.strip('\"'))
+        print(start_frame)
+
+        end_frame = os.environ.get('G_END')
+        end_frame = int(end_frame.strip('\"'))
+        print(end_frame)
 
         
+        cmds.playbackOptions(edit=True,animationStartTime = start_frame)
+        cmds.playbackOptions(edit=True,animationEndTime = end_frame)
+        
+        cmds.playbackOptions(edit=True,minTime = start_frame)
+        cmds.playbackOptions(edit=True,maxTime = end_frame)
         
         
 win = BuildScene()
